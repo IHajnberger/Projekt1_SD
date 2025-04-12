e30a49088a143e1d7835c71072ad2614ae80f422
IHajnberger <280138@student.pwr.edu.pl>
2025-04-12 18:01:49 +0200

    dodanie komentarzy

--- lista_wiazana.cpp
+++ lista_wiazana.cpp
@@ -2,51 +2,51 @@
 #include "lista_wiazana.hpp"
 using namespace std;
 
-Node::Node(int value)
+Node::Node(int value) // konstruktor węzła
 {
-    data = value;
-    next = nullptr;
+	data = value; // ustawienie wartości elementu
+	next = nullptr; // ustawienie wskaźnika do następnego elementu na nullptr
 }
-ListaWiazana::ListaWiazana()
+ListaWiazana::ListaWiazana() // konstruktor listy
 {
     head = nullptr;  //inicjalizacja wskaźnika do pierwszego elementu
     tail = nullptr;     //inicjalizacja wskaźnika do ostatniego elementu
-    count = 0;
+    count = 0;          //pusta lista 
 }
-ListaWiazana::~ListaWiazana()
+ListaWiazana::~ListaWiazana()  // destruktor listy
 {
     Node* current = head;   //wskaźnik do aktualnego elementu
     Node* nextNode;     //wskaźnik do następnego elementu
     while (current != nullptr) 
     {
-        nextNode = current->next; 
-        delete current; 
-        current = nextNode; 
+		nextNode = current->next; // zapamiętanie następnego elementu
+		delete current;  // usunięcie aktualnego elementu
+		current = nextNode;  // przejście do następnego elementu
     }
-    head = nullptr; 
-    tail = nullptr; 
+	head = nullptr;     // ustawienie wskaźnika do pierwszego elementu na nullptr   
+	tail = nullptr;     // ustawienie wskaźnika do ostatniego elementu na nullptr
 }
-bool ListaWiazana::isEmpty()
+bool ListaWiazana::isEmpty() // sprawdzenie czy lista jest pusta
 {
-    return head == nullptr; 
+	return head == nullptr; // sprawdzenie czy wskaźnik do pierwszego elementu jest równy nullptr
 }
 
-void ListaWiazana::addFirst(int value)
+void ListaWiazana::addFirst(int value) // dodawanie na początek
 {
     if (isEmpty())
     {
-        head = new Node(value);
+		head = new Node(value); //jeśli lista jest pusta, dodajemy nowy element jako pierwszy
         tail = head;
     }
     else
     {
-        Node* newNode = new Node(value);
-        newNode->next = head;
-        head = newNode;
+		Node* newNode = new Node(value); //tworzymy nowy element
+		newNode->next = head; // ustawiamy wskaźnik nowego elementu na aktualny pierwszy element
+		head = newNode; // przesuwamy wskaźnik do pierwszego elementu na nowy element
     }  
-    count ++;
+	count++; // zwiększamy licznik elementów
 }
-void ListaWiazana::addLast(int value)
+void ListaWiazana::addLast(int value) // dodawanie na koniec
 {
     if(isEmpty())
     {
@@ -55,42 +55,42 @@ void ListaWiazana::addLast(int value)
     }
     else
     {
-        Node* newNode = new Node(value);
+		Node* newNode = new Node(value); //tworzymy nowy element
         tail->next = newNode; 
         tail = newNode; 
     }
     count ++;
 }
-void ListaWiazana::addAtIndex(int index, int value)
+void ListaWiazana::addAtIndex(int index, int value) // dodawanie na wskazany indeks
 {
-        if (index < 0 || index > count) 
+        if (index < 0 || index > count) // sprawdzenie czy indeks nie wykracza poza zakres listy
         {
             cout << "Indeks poza zakresem" << endl;
             return;
         }
-        if (index == 0) 
+		if (index == 0) // jeśli dodajemy na początek
         {
             addFirst(value);     //dodajemy na początek
             return;
         }
-        if (index == count) 
+		if (index == count) // jeśli dodajemy na koniec
         {
             addLast(value);     //dodajemy na koniec
             return;
         }
         Node* newNode = new Node(value);
         Node* current = head; 
-        for (int i = 0; i < index - 1; i++) 
+		for (int i = 0; i < index - 1; i++) // przechodzimy do elementu przed wskazanym indeksem
         {
             current = current->next;
         }
-        newNode->next = current->next; 
-        current->next = newNode;
+		newNode->next = current->next; // ustawiamy wskaźnik nowego elementu na następny element
+		current->next = newNode; // ustawiamy wskaźnik aktualnego elementu na nowy element
         count++; 
 }
-void ListaWiazana::deleteAtIndex(int index)
+void ListaWiazana::deleteAtIndex(int index) // usuwanie z wskazanego indeksu
 {
-    if(index < 0 || index >= count) 
+	if (index < 0 || index >= count) // sprawdzenie czy indeks nie wykracza poza zakres listy
     {
         cout << "Indeks poza zakresem" << endl;
         return;
@@ -100,13 +100,13 @@ void ListaWiazana::deleteAtIndex(int index)
         deleteFirst(); 
         return;
     }
-    if(index == count - 1) 
+	if (index == count - 1) //jeśli usuwamy ostatni element
     {
         deleteLast(); 
         return;
     }
     Node* current = head;
-    for(int i = 0; i < index - 1; i++) 
+	for (int i = 0; i < index - 1; i++) // przechodzimy do elementu przed wskazanym indeksem
     {
         current = current->next;
     }
@@ -114,9 +114,9 @@ void ListaWiazana::deleteAtIndex(int index)
     current->next = Delete->next; 
     delete Delete; 
 }
-void ListaWiazana::deleteFirst()
+void ListaWiazana::deleteFirst() // usuwanie z początku
 {
-    if (isEmpty()) 
+	if (isEmpty()) // sprawdzenie czy lista jest pusta
     {
         cout << "Lista jest pusta." << endl;
         return;
@@ -130,7 +130,7 @@ void ListaWiazana::deleteFirst()
         tail = nullptr;
     }
 }
-void ListaWiazana::deleteLast()
+void ListaWiazana::deleteLast() // usuwanie z końca
 {
     if (isEmpty()) 
     {
@@ -156,7 +156,7 @@ void ListaWiazana::deleteLast()
     }
     count--;
 }
-void ListaWiazana::show()
+void ListaWiazana::show() // pokazywanie listy
 {
     if (isEmpty()) 
     {
@@ -165,7 +165,7 @@ void ListaWiazana::show()
     }
     Node* current = head; 
     int index = 0; 
-    while (current != nullptr)  
+	while (current != nullptr)  // dopóki nie osiągniemy końca listy
     { 
         cout << "[" << index << "] " << current->data << " "; 
         current = current->next;
@@ -173,7 +173,7 @@ void ListaWiazana::show()
     }
     cout << endl; 
 }
-void ListaWiazana::search(int value)
+void ListaWiazana::search(int value) // szukanie wartości
 {
     if (isEmpty()) 
     {
